43a6d26e3da40c3fee5ce23bae3da05632d2a97d
Olivier Albiez <olivier.albiez@poloper.org>
2017-01-03 18:00:41 +0100

    Refactor LeapYear

--- leapyear/src/main/java/leapyear/LeapYear.java
+++ leapyear/src/main/java/leapyear/LeapYear.java
@@ -2,17 +2,27 @@ package leapyear;
 
 public class LeapYear {
     public static boolean leapYear(int year) {
-
-        if (year % 400 == 0) {
-            return true;
-        }
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
+        if (isTypicalLeapYear(year)) {
+            if (isAtypicalCommon(year)) {
+                if (isAtypicalLeapYear(year)) {
+                    return true;
+                }
                 return false;
             }
             return true;
         }
-
         return false;
+    }
+
+    private static boolean isTypicalLeapYear(int year) {
+        return year % 4 == 0;
+    }
+
+    private static boolean isAtypicalCommon(int year) {
+        return year % 100 == 0;
+    }
+
+    private static boolean isAtypicalLeapYear(int year) {
+        return year % 400 == 0;
     }
 }
